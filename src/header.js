789d46eef4e6934a20b9b976fa232af5e7111476
SemStassen <semstassen@gmail.com>
2023-07-21 18:08:43 +0200

    Add default home page

---
+++ src/header.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function header() {
+  const header = document.createElement("header");
+  const container = document.createElement("div");
+  container.classList.add("container", "mx-auto");
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 "use strict";
 
-console.log("this is a log");
+import "./style.css";
